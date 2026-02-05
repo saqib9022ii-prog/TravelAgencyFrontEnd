3b1d1ecc1db86a37298858b72329bf969a29b66d
saqib9022ii-prog <saqib9022ii@gmail.com>
2026-02-05 12:29:47 +0500

    fixes css layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import {Provider} from "react-redux";
 import { store } from "./store/store";
 import App from './App.jsx';
+import "./styles/index.css";
 
 
 
@@ -17,6 +18,5 @@ createRoot(document.getElementById('root')).render(
     </Provider>
   </StrictMode>,
 );
-
 
 
